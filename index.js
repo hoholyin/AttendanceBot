d9383106549a2052fd5563183b5fe10a4b441b3d
hoholyin <holyin97@gmail.com>
2019-09-09 08:53:29 +0800

    update google auth

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 const TelegramBot = require('node-telegram-bot-api');
-const API_TOKEN = process.env.TOKEN; 
+const API_TOKEN = process.env.TOKEN;
 const telegram = new TelegramBot(API_TOKEN, { polling: true });
 
 const {google} = require('googleapis');
-const fs = require('fs');
-const readline = require('readline');
-const promisify = require('es6-promisify');
+const authentication = require("./authentication");
 
-const TOKEN_PATH = 'token.json';
-const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
 const sheets = google.sheets('v4');
 const SHEET_ID = "1_tNYZINkyw9PItP4PXEulnmggCW6-NY98wKpIuUk3pY";
 
 const PRESENT = 1;
 
-let values = [[PRESENT]];
-let resource = {
-  values,
-};
-
 telegram.on("text", (msg) => {
   if (msg.text == "/start") {
-    sendWelcomeMessage(msg);
+    sendMessage(msg, "yo");
   } else {
-    markAsPresent(msg);
+    authentication.authenticate().then((auth) => {
+      sendMessage(msg, getData(auth));
+    });
   }
 });
+
+function sendMessage(msg, text) {
+  try {
+    telegram.sendMessage(msg.chat.id, text);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 function sendWelcomeMessage(msg) {
   try {
@@ -38,6 +39,27 @@ function sendWelcomeMessage(msg) {
     console.log(error);
   }
 }
+
+function getData(auth) {
+  sheets.spreadsheets.values.get({
+    auth: auth,
+    spreadsheetId: SHEET_ID,
+    range: 'Results!O4',
+  }, (err, response) => {
+    if (err) {
+      console.log("The API returned an error: ' + err");
+      return;
+    }
+    let rows = response.values;
+    if (rows.length === 0) {
+      return 0;
+    } else {
+      let result = rows[0];
+      return result;
+    }
+  });
+}
+
 
 function markAsPresent(msg) {
   sheets.spreadsheets.values.update({
